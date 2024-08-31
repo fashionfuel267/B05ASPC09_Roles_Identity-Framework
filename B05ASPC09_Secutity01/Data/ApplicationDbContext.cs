d61c98a4e90d99ea0566ea7da918bb8e5bd7fddf
Shahanaj <fashionfuel267@gmail.com>
2024-08-31 16:47:07 +0600

    Add project files.

---
+++ B05ASPC09_Secutity01/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace B05ASPC09_Secutity01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
